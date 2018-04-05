af927475fbcd16c6a142a8b5337aad0b8b7b87c9
sergioalmeida33 <37555278+sergioalmeida33@users.noreply.github.com>
2018-04-05 00:12:56 -0300

    RESTO DA DIVISAO

---
+++ restodadivisao/src/restodadivisao/restodadivisao.java
@@ -0,0 +1,35 @@
+package restodadivisao;
+
+import java.util.Scanner;
+
+public class restodadivisao {
+public static void main (String[]args){
+	Scanner in = new Scanner (System.in);
+	int num;
+	num = 0;
+	num = 1;
+System.out.println(6 % 2);
+if      (num==0){
+	System.out.println("o resultado e par");
+	
+}else if
+        (num==1)  {
+	System.out.println("o resultado e impar");
+}
+
+// simbolo de %%% nao e para porcentagem e para diser
+//o resto de uma conta 
+System.out.println(4 % 2);
+System.out.println(5 % 2);
+System.out.println(6 % 2);
+System.out.println(7 % 3.1);
+System.out.println("o resultado da operaçao e " );
+	
+	
+	
+	
+	
+	
+	
+}
+}
