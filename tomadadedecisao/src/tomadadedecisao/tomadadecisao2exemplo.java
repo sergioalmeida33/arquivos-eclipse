b93600b49a101e9605d94bc2b3f22af79c3b1bc0
sergioalmeida33 <37555278+sergioalmeida33@users.noreply.github.com>
2018-04-05 00:13:35 -0300

    TOMADA DE DESISAO 2

---
+++ tomadadedecisao/src/tomadadedecisao/tomadadecisao2exemplo.java
@@ -0,0 +1,27 @@
+package tomadadedecisao;
+import java.util.Scanner;
+
+public class tomadadecisao2exemplo {
+	public static void main (String[]args){
+		int num;
+		System.out.println("digite o  um numero  ");
+		Scanner in = new Scanner (System.in);
+		
+		num = in.nextInt();
+		if (num == 1){
+			System.out.println("obrigado por digitar o numero 1");
+		}
+		else{
+			System.out.println("o numero digitado nao e numero um");
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+	}
+}
