c0ff10570c5f9308ec32ff70dadf695758de35fd
sergioalmeida33 <37555278+sergioalmeida33@users.noreply.github.com>
2018-04-05 00:03:44 -0300

    AULA3

---
+++ aula3/src/com/excript/aula3/aula3.java
@@ -0,0 +1,18 @@
+package com.excript.aula3;
+
+public class aula3 {
+	
+	public static void main(String[]args){
+		
+		int num1,num2;
+	    num1=100;
+		num2 = num1+100;
+	
+		System.out.println("o resultado e: "+ num2);
+		System.out.println("Num1 e igual: "+num1);
+		
+		
+		
+	}
+
+}
