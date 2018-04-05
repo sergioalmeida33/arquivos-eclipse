9bd277d2eda60a44fea5389714c7d077fb046b5b
sergioalmeida33 <37555278+sergioalmeida33@users.noreply.github.com>
2018-04-05 00:14:21 -0300

    TOMADA DE DECISAO 2

---
+++ tomadadedecisao2/src/tomadadedecisao2/tomadadedecisao2.java
@@ -0,0 +1,18 @@
+package tomadadedecisao2;
+
+import java.util.Scanner;
+public class tomadadedecisao2 {
+	public static void main (String[]args){
+		
+	System.out.println(1==1);
+		// sempre que quiser armazenar o resultado 
+	// de uma condiçao declara boolean//
+	boolean b=  2!=1;
+	// negando a condiçao colocar ! exclamaçao
+	//um e diferente de um
+	//boolean b=  1!=1; 1 exemplo//
+	System.out.println(b );
+	
+		
+	}
+}
