2bbcbbf33630854ffd88f1df13db671071f3bda6
sergioalmeida33 <37555278+sergioalmeida33@users.noreply.github.com>
2018-04-05 00:14:45 -0300

    USUARIO DIGITA

---
+++ usuariodigita/src/usuariodigita/usuariodigita.java
@@ -0,0 +1,15 @@
+package usuariodigita;
+import java.util.Scanner;
+
+public class usuariodigita {
+	public static void main(String[]args){
+	//imprime um numero	
+	System.out.println("digite um numero");
+	Scanner in = new Scanner(System.in);
+	System.out.println(in.nextLine() );
+		
+		
+	}
+
+}
+
